48372267acdf6ec69dbf2f7bb74ae99d5f6c220d
vinceajcs <hvincentstephen@gmail.com>
2018-10-12 19:28:41 -0700

    add event listener condition when user clicks on number key

--- calculator.js
+++ calculator.js
@@ -1,13 +1,25 @@
 const calculator = document.querySelector('.calculator')
+const display = document.querySelector('.calculator__display')
 const keys = calculator.querySelector('.calculator__keys')
+
 
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target
     const action = key.dataset.action
 
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+
     if (!action) {
-      console.log('number key!')
+      /* if current display number is 0,
+      next number clicked should replace 0
+      otherwise, append the next number clicked to the current display number */
+      if (displayedNum === '0') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent
+      }
     }
 
     if (action === 'add' ||
